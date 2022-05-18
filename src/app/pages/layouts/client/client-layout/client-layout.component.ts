ca559e6622461176f899ffcb4983df69ec456daf
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-05-18 17:40:35 +0700

    update css

--- src/app/pages/layouts/client/client-layout/client-layout.component.ts
+++ src/app/pages/layouts/client/client-layout/client-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-client-layout',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientLayoutComponent implements OnInit {
 
+  scrollTop!: number
+
   constructor() { }
 
   ngOnInit(): void {
+    window.onscroll = () => this.scrollTop = window.scrollY;
   }
 
 }
--- src/app/pages/layouts/client/footer-client/footer-client.component.ts
+++ src/app/pages/layouts/client/footer-client/footer-client.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer-client',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterClientComponent implements OnInit {
 
+  @Input() scrollTop!: number
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  backToTop() {
+    window.scroll({
+      top: 0,
+      behavior: 'smooth'
+    })
   }
 
 }
